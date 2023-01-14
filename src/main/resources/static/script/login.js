382e9979c509e0b3c565c5e93fb6c338dc7b51c9
ghlee0303 <rjs993530@gmail.com>
2023-01-14 14:04:59 +0900

    fix: 로그인 인증 처리 변경
    1. 복잡했던 코드를 개선합니다.
    2. fetchHandler.js를 import 받아와 HttpsStatus에 맞는 처리를 합니다.

--- src/main/resources/static/script/login.js
+++ src/main/resources/static/script/login.js
@@ -1,9 +1,11 @@
+import * as fetchHandler from 'fetchHandler';
+
 window.onload = function () {
     eventBinding();
 }
 
 function loginProcess() {
-    const serverUri = "http://localhost:8080/loginProc";
+    const serverUri = "/loginProc";
     const email = document.getElementById("email").value;
     const pwd = document.getElementById("pwd").value;
     const options = {
@@ -14,36 +16,14 @@ function loginProcess() {
     }
 
     fetch(serverUri, options)
-        .then(res =>
-            loginCheck(res)
-        )
-        .then(loginCode =>
-            loginAlert(loginCode)
-        );
-}
-
-
-function loginCheck(res) {
-    return new Promise(function(resolve, reject) {
-        if (res.status == 401) {
-            resolve(res.json())
-        }
-        resolve(100);
-    });
-}
-
-function loginAlert(code) {
-    switch (code) {
-        case 100 :
-            location.href="/";
-            break;
-        case 600 :
-            alert("알 수 없는 이유로 로그인이 안되고 있습니다.");
-            break;
-        case 601 :
-            alert("아이디 또는 비밀번호가 맞지 않습니다.");
-            break;
-    }
+        .then((res) => fetchHandler.toJsonPromise(res))
+        .then((res) => fetchHandler.httpStatusHandler(res) )
+        .then((res) => {
+            location.href = "/";
+        })
+        .catch((err) => {
+            alert(err.res.data.message);
+        });
 }
 
 function eventBinding() {
